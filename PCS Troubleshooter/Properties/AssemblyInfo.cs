7fe59c6cdd6c8eee4ed52ec869d8edf690a9b2cb
Alexander Ellingsen <alexellingsen@gmail.com>
2014-01-24 14:49:14 +0100

    Fill in assembly information

--- PCS Troubleshooter/Properties/AssemblyInfo.cs
+++ PCS Troubleshooter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PCS Troubleshooter")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Alexander Ellingsen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
